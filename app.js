2a503e52bd5a637c7fede9541b54434cc0db6576
Francis Gomez <rigoyots@gmail.com>
2021-04-03 22:17:58 +0800

    Put the google authentication inside app.js from server.js

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 // app.js template
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path")
+const path = require("path");
+const passport = require('passport');
+const googleStratergy = require('./googleStrategy')
 
 const app = express();
 
+app.use(passport.initialize())
+
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-    res.render("index");
+// app.get('/', function (req, res) {
+//     res.render("index");
+// });
+
+app.get('/', passport.authenticate('google', {
+    scope: ['email', 'profile']
+}), (req, res) => {
+
+});
+
+app.get('/callback', passport.authenticate('google', {
+    scope: ['email', 'profile']
+}), (req, res) => {
+    return res.send("Congrats");
 });
 
 app.listen(3000, function () {
